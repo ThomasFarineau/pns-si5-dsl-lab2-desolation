17604ebb2369757d91519318e3ed9351c2d187e1
Thomas <thomas.farineau@icloud.com>
2024-01-18 14:07:35 +0100

    début rapport + readme pour la mise en oeuvre

--- bash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ./kernel && npm run build
-cd ../midi-dsl && npm run reload:kernel && npm run langium:generate && npm run build
--- kernel/src/example.ts
+++
@@ -1,8 +0,0 @@
-import {build} from "./index";
-import * as Path from "path";
-import * as fs from "fs";
-
-fs.readFile(Path.resolve(__dirname, './music.json'), (err, data) => {
-    if (err) throw err;
-    build(data.toString());
-});
